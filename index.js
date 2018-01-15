3237040509369b9f9daa76a3a7b581e35a38e2c3
steven <langsford.steven@gmail.com>
2018-01-14 20:07:12 -0500

    first draft member survey

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ const helmet = require('helmet'); //minimal security best practices. Sets HTTP h
 const favicon = require('serve-favicon')
 
 //App settings:
-app.use(favicon(__dirname + '/public/psi.ico')); //You might want to change this.
+app.use(favicon(__dirname + '/public/byc.ico')); //You might want to change this.
 
 app.use(session({
   cookieName: 'session',
-  secret: 'not_the_actual_secret',
+  secret: 'myp22etr5a2bbit2sb6arki3n4gisgo0n0n9awak3et7he5h3ors1ei5n4theb1aseme24ndt',
   duration: 30 * 60 * 1000,
   activeDuration: 5 * 60 * 1000,
 }));
@@ -39,7 +39,7 @@ function requireLogin (request, response, next) {
 };
 
 app.post('/login',function(req,res){
-    if(req.body.token=="keys")req.session.authtoken=true; //set session token
+    if(req.body.token=="headlessillawonga")req.session.authtoken=true; //set session token
     return res.status(200).send("/dashboard"); //dashboard checks if session token is set
 //    res.render("pages/dashboard",{auth:"true"});
 });
@@ -55,50 +55,53 @@ app.get("/dashboard",function(req,res){
 
 app.get("/getresponses",requireLogin,function(req,res){
     var pool = new pg.Pool({connectionString:process.env.DATABASE_URL});
+    console.log("flag1");
     pool.connect(function(err,client,done){
+	console.log("flag2");
 	client.query('select * from responses',function(err,result){
 	    if(err){
-		{console.error(err); res.send("Error "+err);}
-		}else{
+		{console.error("flag3"+err); res.send("Error "+err);}
+	    }else{
+		console.log("flag4")
 		    //TODO do something sensible if there are no results!
-		    var fields = Object.keys(JSON.parse(result.rows[0].responseobj));
-		    var responses = [];
-		    	   for(var i=0;i<result.rowCount;i++){
-			       responses.push(JSON.parse(result.rows[i].responseobj));
-			   }
-
-		    var response_csv = json2csv({data: responses, fields:fields});
+		var fields = ["time","sessionid","response"];
+		var responses = [];
+		for(var i=0;i<result.rowCount;i++){
+		    responses.push({time:result.rows[i].time, sessionid:result.rows[i].sessionid,response:result.rows[i].response});
+		}
+		    var response_csv = json2csv({data: responses, fields:fields}); //this may be an unholy way to create a csv. It evolved. Sorry.
 		    res.attachment("responsedata.csv");
 		    res.send(response_csv);
 		}
 	});//end query
     });
-    pool.end();    
+    pool.end();
+    console.log("flag 5")
 });
 
 
 //should collapse this and getresponses into one getData and pass it the target table name? Two issues with that, differing col names in the db and getting file-save prompts from the client page.
-app.get("/getdemographics",requireLogin,function(req,res){
-    var pool = new pg.Pool({connectionString:process.env.DATABASE_URL});
-    pool.connect(function(err,client,done){
-	client.query('select * from demographics',function(err,result){
-	    if(err){
-		{console.error(err); res.send("Error "+err);}
-		}else{
-		    //TODO do something sensible if there are no results!
-		    var fields = Object.keys(JSON.parse(result.rows[0].demoobj));
-		    var responses = [];
-		    	   for(var i=0;i<result.rowCount;i++){
-			       responses.push(JSON.parse(result.rows[i].demoobj));
-			   }
-		    var response_csv = json2csv({data: responses, fields:fields});
-		    res.attachment("demographicsdata.csv");
-		    res.send(response_csv);
-		}
-	});//end query
-    });
-    pool.end();    
-});
+// app.get("/getdemographics",requireLogin,function(req,res){
+//     var pool = new pg.Pool({connectionString:process.env.DATABASE_URL});
+//     pool.connect(function(err,client,done){
+// 	client.query('select * from demographics',function(err,result){
+// 	    if(err){
+// 		{console.error(err); res.send("Error "+err);}
+// 		}else{
+// 		    //TODO do something sensible if there are no results!
+// 		    var fields = Object.keys(JSON.parse(result.rows[0].demoobj));
+// 		    var responses = [];
+// 		    	   for(var i=0;i<result.rowCount;i++){
+// 			       responses.push(JSON.parse(result.rows[i].demoobj));
+// 			   }
+// 		    var response_csv = json2csv({data: responses, fields:fields});
+// 		    res.attachment("demographicsdata.csv");
+// 		    res.send(response_csv);
+// 		}
+// 	});//end query
+//     });
+//     pool.end();    
+// });
 
 
 
@@ -108,13 +111,9 @@ app.get('/', function (req, res) {
    
 })
 
-app.get('/run',function(req,res){
-    res.render("pages/exp");
+app.get('/survey',function(req,res){
+    res.render("pages/survey");
 });
-app.post('/exp',function(req,res){
-    //not sure why this needs to bounce to a new route to render, but it does seem to. I need to learn some express!
-    return res.status(200).send("/run");    
-})
 
 app.get('/done',function(req,res){
     res.render("pages/outro");
@@ -123,29 +122,29 @@ app.post('/finish',function(req,res){
     return res.status(200).send("/done");//Another instance of the double-bounce to render. Why?
 });
 
-//Paricipant data-saving routes, push stuff to the database (but don't render new pages.)
-app.post('/demographics',function(req,res){
-//save the response in db
-    var pool = new pg.Pool(
-	{connectionString:process.env.DATABASE_URL}
-    )    
-    // connection using created pool
-    pool.connect(function(err, client, done) {
-    	client.query('insert into demographics values ($1, $2)', //Probably a crime to save a multi-value objs in one 'info' col. Oh well.
-		     [Date.now(),
-		     req.body.demographics],
-    		     function(err, result){
-    			 if (err)
-    			 {console.error(err); res.send("Error " + err); } //For now the client just prints the error to the console. What's ideal?
-    			 else
-    			 { // response.render('pages/db', {results: result.rows});
-    			     res.send("success");
-    			 }
-    		     });//end query
-	done();
-    });
-    pool.end()
-});
+//Participant data-saving routes, push stuff to the database (but don't render new pages.)
+// app.post('/demographics',function(req,res){
+// //save the response in db
+//     var pool = new pg.Pool(
+// 	{connectionString:process.env.DATABASE_URL}
+//     )    
+//     // connection using created pool
+//     pool.connect(function(err, client, done) {
+//     	client.query('insert into demographics values ($1, $2)', //Probably a crime to save a multi-value objs in one 'info' col. Oh well.
+// 		     [Date.now(),
+// 		     req.body.demographics],
+//     		     function(err, result){
+//     			 if (err)
+//     			 {console.error(err); res.send("Error " + err); } //For now the client just prints the error to the console. What's ideal?
+//     			 else
+//     			 { // response.render('pages/db', {results: result.rows});
+//     			     res.send("success");
+//     			 }
+//     		     });//end query
+// 	done();
+//     });
+//     pool.end()
+// });
 
 
 app.post('/response',function(req,res){
@@ -155,9 +154,10 @@ app.post('/response',function(req,res){
     )    
     // connection using created pool
     pool.connect(function(err, client, done) {
-    	client.query('insert into responses values ($1,$2)', //NOTE this assumes table responses exists with cols 'time', 'responseobj' !
+    	client.query('insert into responses values ($1,$2,$3)', //NOTE this assumes table responses exists with cols 'time', 'ppntid' 'response_string'
 		     [Date.now(),
-		     req.body.myresponse],
+		      req.body.sessionID,
+		      req.body.myresponse],
     		     function(err, result){
     			 if (err)
     			 {console.error(err); res.send("Error " + err); } //For now the client just prints the error to the console. What's ideal?
--- public/js/exp.js
+++ public/js/exp.js
@@ -1,25 +1,23 @@
-//Generic sequence-of-trials
-//If that's all you want, all you need to edit is the makeTrial object and the responseListener. Give maketrial an appropriate constructor that accept the key trial properties, a drawMe function, and something that will hit responseListener.
-//then put a list of trial-property-setter entries in 'stim' and you're golden.
+var ppntID = Math.round(Math.random()*10000000);
+localStorage.setItem("ppntID",ppntID); //cookie alternative, retrive with localStorage.getItem("ppntID"). Only stores strings. Privacy-preserving session tagging.
 
-var trials = [];
 var trialindex = 0;
 
 function responseListener(aresponse){//global so it'll be just sitting here available for the trial objects to use. So, it must accept whatever they're passing.
 //    console.log("responseListener heard: "+aresponse); //diag
-    trials[trialindex].response = aresponse;
-    trials[trialindex].responseTime= Date.now();
     
-    $.post('/response',{myresponse:JSON.stringify(trials[trialindex])},function(success){
-    	console.log(success);//For now server returns the string "success" for success, otherwise error message.
+    $.post('/response',{myresponse:aresponse,sessionID:localStorage.getItem("ppntID")},function(success){
+    	console.log("Logging "+aresponse+":"+success);//For now server returns the string "success" for success, otherwise error message.
     });
     
-    //can put this inside the success callback, if the next trial depends on some server-side info.
+    // //can put this inside the success callback, if the next trial depends on some server-side info.
+    //    console.log(aresponse);//dev version
     trialindex++; //increment index here at the last possible minute before drawing the next trial, so trials[trialindex] always refers to the current trial.
     nextTrial();
 }
 
 function nextTrial(){
+    document.getElementById("surveyprogress").innerHTML="Question "+(trialindex+1)+" of "+trials.length;
     if(trialindex<trials.length){
 	trials[trialindex].drawMe("uberdiv");
     }else{
@@ -27,36 +25,117 @@ function nextTrial(){
     }
 }
 
-// a trial object should have a drawMe function and a bunch of attributes.
-//the data-getting process in 'dashboard.ejs' & getData routes creates a csv with a col for every attribute, using 'Object.keys' to list all the properties of the object. Assumes a pattern where everything interesting is saved to the trial object, then that is JSONified and saved as a response.
-//Note functions are dropped by JSON.
-//Also note this means you have to be consistent with the things that are added to each trial before they are saved, maybe init with NA values in the constructor.
-function makeTrial(questiontext){
-    this.ppntID = localStorage.getItem("ppntID");
-    this.questiontext = questiontext;
-    this.drawMe = function(targdiv){
-	this.drawTime = Date.now();
-	var responses = "<button onclick='responseListener(\"yes\")'>Yes</button><button onclick='responseListener(\"no\")'>No</button>";
-	document.getElementById(targdiv).innerHTML=
-	    "<div class='trialdiv'><p>"+this.questiontext+"</br>"+responses+"</p></div>";
-    }
+
+var scrapeAnswers; //each question constructor should set this to a function that collects all the responses, whether they're text or check boxes or whatever qspecific thing, and passes them as one string to responseListener.
+//repsonseListener then pushes the string to the database 'responses' table.
+var progressButtons = "<br/><button onclick='scrapeAnswers()'>Next</button>"
+
+//constructors for different question types follow: all MUST HAVE a 'drawMe' function, which MUST SET an appropriate scrapeanswers function that passes a string to responseListener, and have something that calls scrapeAnswers (probably progressButtons, but hey, could be something else).
+
+function textbox_question(qtext){ //qtext is a string
+	 this.drawMe = function(mydiv){
+	     document.getElementById(mydiv).innerHTML = "<h3 class='qmain'>"+qtext+"</h3><br/><textarea id='textboxQ' style='width:500px; class=\"response\"'></textarea>"+progressButtons;
+
+	     scrapeAnswers = function(){
+		 responseListener(document.getElementById('textboxQ').value);
+	     }
+	 }
+}
+
+function textboxlist_question(qmain,labellist){ //qmain is a string, labellist is an array of strings
+    this.drawMe = function(mydiv){
+	var drawstring = "<h3 class='qmain'>"+qmain+"</qmain></br>";
+	for(var i=0;i<labellist.length;i++){
+	    drawstring+=labellist[i]+"&nbsp<textarea id='qlist"+i+"' style='width:500px; class=\"response"+i+"\"'></textarea></br>";
+	}
+	drawstring+=progressButtons;
+	scrapeAnswers = function(){
+	    var responseString = qmain+" :: ";
+	    for(var i=0;i<labellist.length;i++){
+		responseString+=labellist[i]+":"+document.getElementById('qlist'+i).value+"_";
+	    }
+	    responseListener(responseString);
+	}
+ 	document.getElementById(mydiv).innerHTML = drawstring;
+    }//end drawme
+}//end textboxlist
+
+function dropdownlist_question(qmain,qoptions){
+     this.drawMe = function(mydiv){
+	 var drawstring = "<h3 class='qmain'>"+qmain+"</h3></br><select id='ddlist'>";
+	 for(var i=0;i<qoptions.length;i++){
+	     drawstring+="<option value='"+qoptions[i]+"'>"+qoptions[i]+"</option>";
+	 }
+	 drawstring+="</select></br>"+progressButtons;
+
+	 scrapeAnswers = function(){
+	     responseListener(document.getElementById('ddlist').value);
+	 }
+	 document.getElementById(mydiv).innerHTML = drawstring;
+     }
+}
+
+function checkboxes_question(qmain,qoptions){
+     this.drawMe = function(mydiv){
+	 var drawstring = "<h3 class='qmain'>"+qmain+"</h3></br>";
+	 for(var i=0;i<qoptions.length;i++){
+	     drawstring+="<input type='checkbox' id='checkq"+[i]+"' value='"+qoptions[i]+"'>"+qoptions[i]+"</br>";
+	 }
+	 drawstring+="Other&nbsp<textarea id='otherans' style='width:500px; class=\"response"+i+"\"'></textarea></br>";
+	 drawstring+="</br>"+progressButtons;
+
+	 scrapeAnswers = function(){
+	     var responseString = qmain+"::";
+	     for(var i=0;i<qoptions.length;i++){
+		 if(document.getElementById('checkq'+i).checked)responseString+=document.getElementById('checkq'+i).value+"_";
+	     }
+	     if(document.getElementById('otherans').value.length>0)responseString+=" other "+document.getElementById("otherans").value;
+	     responseListener(responseString);
+	 }
+	 document.getElementById(mydiv).innerHTML = drawstring;
+     }
 }
 
 
 
-function shuffle(a) { //via https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
-    var j, x, i;
-    for (i = a.length - 1; i > 0; i--) {
-        j = Math.floor(Math.random() * (i + 1));
-        x = a[i];
-        a[i] = a[j];
-        a[j] = x;
-    }
-    return a;
-}
+// function shuffle(a) { //via https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
+//     var j, x, i;
+//     for (i = a.length - 1; i > 0; i--) {
+//         j = Math.floor(Math.random() * (i + 1));
+//         x = a[i];
+//         a[i] = a[j];
+//         a[j] = x;
+//     }
+//     return a;
+// }
 //****************************************************************************************************
 //Stimuli
-var stim = shuffle(["Does this question have a correct answer?","What is the correct answer to this question?"]);
-trials = stim.map(function(x){return new makeTrial(x)});
+var trials = [new textbox_question("How did you find out about Burnside Youth Club?"),
+	      new textbox_question("What do you consider to be our club's strengths?"),
+	      new textbox_question("How many other sporting / artistic / educational sessions do you attend after hours or on weekends each week?"),
+	      new textbox_question("Do you have any suggestions that would help our club to attract more new members?"),
+	      new textbox_question("Do you have any suggestions that would help our club to keep current members longer? "),
+	      new textbox_question("Is there anything you would like our club to do differently? "),
+	      new textboxlist_question("Where would you prefer our training venue to be located? (Name four preferred suburbs).",["Preference 1","Preference 2","Preference 3","Preference 4"]),
+	      new textbox_question("Which training activities do you enjoy the most?"),
+	      new textbox_question("What do you need to work on the most to improve your skills in this sport?"),
+	      new textbox_question("What do you like about this sport?"),
+	      new textbox_question("What don't you like about this sport?"),
+	      new textboxlist_question("How many training sessions would you prefer to attend each week?",["Maximum","Minimum"]),
+	      new dropdownlist_question("Which one of the following levels of achievement best indicates your goal in this sport?",
+					["Achieving a few key skills as a recreational participant",
+					 "Achieving a wide range of skills as a recreational participant",
+					 "Compete as a club team member in South Australian competitions",
+					 "Represent South Australia at the Australian Championships",
+					 "Represent Australia in international competitions"
+					]),
+	      new checkboxes_question("Are you interested in undertaking other roles in the sport either now or in the future? (Select any that apply)",
+				      ["Coaching",
+				       "Judging/refereeing",
+				       "Administration",
+				       "Management"]),
+	      new textbox_question("Is there any other comment or suggestion you'd like to make?")
+	     ];
+//trials = stim.map(function(x){return new makeTrial(x)});
 
-nextTrial();
+//nextTrial(); //now on a button on survey.ejs page, which has  preamble message.
